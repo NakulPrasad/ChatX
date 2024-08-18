1ba55d9728613ac9ded66128b2305ab5e9a504ae
Nakul Prasad <nakulprasad10@outlook.com>
2024-08-18 05:35:52 +0530

    fix for deployment

--- backend/sockets/SocketIoSingleton.js
+++ backend/sockets/SocketIoSingleton.js
@@ -1,6 +1,5 @@
 const { Server } = require('socket.io');
 const SocketManager = require('./SocketManager');
-const ORIGINS = process.env.ORIGINS.split('|');
 class SocketIoSingleton {
     constructor(httpServer) {
         if (SocketIoSingleton.instance) {
@@ -9,7 +8,11 @@ class SocketIoSingleton {
 
         this.io = new Server(httpServer, {
             cors: {
-                origin: ORIGINS,
+                origin: [
+                    "http://localhost:5173",
+                    "https://current-bridie-nakul-384e1fc1.koyeb.app",
+                    "https://nakulprasad.me"
+                ],
                 methods: ["GET", "POST"]
             },
         });
